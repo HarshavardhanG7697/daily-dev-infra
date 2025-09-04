57667e59a2504f7c2ac6cdb793eaf6f19f3179e1
Harshavardhan Gowda <harsha7697@gmail.com>
2025-09-04 07:30:29 +0000

    Removing VPC and adding Elasticache clusters

--- main.tf
+++ main.tf
@@ -1,14 +1,51 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_elasticache_replication_group" "redis-cmd" {
+  automatic_failover_enabled = true
+  multi_az_enabled           = true
+  replication_group_id       = "elasticache-learning-cmd"
+  description                = "A cluster mode disabled redis cluster for testing"
+  cluster_mode               = "disabled"
+  engine                     = "redis"
+  engine_version             = "7.1"
+  node_type                  = "cache.t4g.micro"
+  num_cache_clusters         = 2
+  parameter_group_name       = "default.redis7"
+  port                       = 6379
+  subnet_group_name          = "skyforge-subnet-group"
+  security_group_ids         = ["sg-07597827c4cfe1432"]
+}
 
-  name = "${local.name_prefix}-vpc"
-  cidr = "10.0.0.0/16"
+resource "aws_elasticache_serverless_cache" "redis-serverless" {
+  engine = "valkey"
+  name   = "elasticache-learning-serverless"
+  cache_usage_limits {
+    data_storage {
+      maximum = 1
+      unit    = "GB"
+    }
+    ecpu_per_second {
+      maximum = 1000
+    }
+  }
+  description              = "Serverless cache for testing purpose"
+  major_engine_version     = "8"
+  snapshot_retention_limit = 1
+  subnet_ids               = ["subnet-0da96ed78aad5112a", "subnet-0825cfaba77828b79", "subnet-0fd27a11576a595f4"]
+  security_group_ids       = ["sg-07597827c4cfe1432"]
+}
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_dns_hostnames    = true
-  enable_dns_support      = true
-  map_public_ip_on_launch = true
+resource "aws_elasticache_replication_group" "redis-cme" {
+  automatic_failover_enabled = true
+  multi_az_enabled           = true
+  replication_group_id       = "elasticache-learning-cme"
+  description                = "A cluster mode disabled redis cluster for testing"
+  cluster_mode               = "enabled"
+  engine                     = "valkey"
+  engine_version             = "8.1"
+  node_type                  = "cache.t4g.micro"
+  num_node_groups            = 2
+  replicas_per_node_group    = 1
+  parameter_group_name       = "default.valkey8.cluster.on"
+  port                       = 6379
+  subnet_group_name          = "skyforge-subnet-group"
+  security_group_ids         = ["sg-07597827c4cfe1432"]
 }
